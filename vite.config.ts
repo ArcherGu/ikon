b64ec3a7cd5f7461c6c6f624880de8b9b5d81908
archergu <649601376@qq.com>
2024-03-25 14:23:57 +0800

    feat: add element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import UnoCss from 'unocss/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5178,
+  },
   plugins: [
     Vue({
       script: {
@@ -16,8 +21,12 @@ export default defineConfig({
     }),
     UnoCss(),
     Icons(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
     Components({
       resolvers: [
+        ElementPlusResolver(),
         IconsResolver(),
       ],
     }),
